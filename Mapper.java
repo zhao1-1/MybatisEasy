8512e6d482d7330510d7376d554a82c16114e2e4
zhao1-1 <55967581+zhao1-1@users.noreply.github.com>
2020-10-22 12:49:34 +0800

    Create Mapper.java

---
+++ Mapper.java
@@ -0,0 +1,9 @@
+package org.apache.ibatis.cfg;
+
+// 用于封装执行的SQL语句和结果类型的全限定类名
+public class Mapper {
+    private String queryString;		// SQL语句
+    private String resultType;		// 结果封装实体类的全限定类名
+    
+    // 省略get、set方法
+}
